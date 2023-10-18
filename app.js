1e9e7de6ade120c8ec0769cb0a68b16751ba3486
DaviiJones <davonbridgett@pursuit.org>
2023-10-17 21:54:13 -0400

    server is working

--- app.js
+++ app.js
@@ -3,5 +3,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors")
 
+app.use(express.json());
+app.get("/",(req, res) => {
+    res.send('Welcome to my Budgeting App!');
+});
+app.get("*", (req, res) => {
+    res.status(404).json({error: "Sorry! No page found"})
+});
+
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require("./app.js");
 
 require("dotenv").config();
-
 const PORT = process.env.PORT || 1313;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
