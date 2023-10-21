19fc6e51a79233349ba49878837c332e8b6feead
DaviiJones <davonbridgett@pursuit.org>
2023-10-21 00:53:19 -0400

    middleware is responding

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require("express");
+const transaction = require("./controllers/transactionController");
 
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
 
 app.use(express.json());
+app.use("/transaction", transaction);
 
+// const coolest = "Gen";
 
+app.get("/", (req, res) => {
+   
+    res.send(`Welcome to my Budgeting App`);
+})
 
-app.get("/",(req, res) => {
-    res.send('Welcome to my Budgeting App!');
-});
-app.get('/transactionData', (req, res) => {
-    res.json(dummyTransactionData);
-  });
-  
 app.get("*", (req, res) => {
-    res.status(404).json({error: "Sorry! No page found"})
-});
+    res.status(404).json({error: "no page found "})
+})
+
 
 
 module.exports = app;
